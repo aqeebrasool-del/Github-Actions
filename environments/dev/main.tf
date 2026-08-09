c73a19a405df474671cce496e1e879ca2970faa1
aqeeb <aqeebrasool@gmail.com>
2026-08-09 11:01:19 +0530

    initial commit

---
+++ environments/dev/main.tf
@@ -0,0 +1,34 @@
+module "resource_group" {
+  source         = "../../modules/azurerm_resource_group"
+  resource_group = var.resource_group
+
+}
+
+module "virtual_network" {
+  source     = "../../modules/azurerm_vnet"
+  depends_on = [module.resource_group]
+  vnets      = var.virtual_network
+
+}
+
+module "subnets" {
+  source     = "../../modules/azurerm_subnet"
+  depends_on = [module.virtual_network]
+  subnets    = var.subnets
+
+}
+
+
+module "public_ip" {
+  source     = "../../modules/azurerm_public_ip"
+  depends_on = [module.resource_group]
+  public_ip  = var.public_ip
+
+}
+
+module "virtual_machine" {
+  source          = "../../modules/azurerm_virtual_machine"
+  depends_on      = [module.public_ip, module.subnets]
+  virtual_machine = var.virtual_machine
+}
+
