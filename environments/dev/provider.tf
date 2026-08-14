c091edb3034a34a7bf8487c29c2dbbbe9a4de3f5
aqeeb <aqeebrasool@gmail.com>
2026-08-14 18:24:41 +0530

    added backend blcok

--- environments/dev/provider.tf
+++ environments/dev/provider.tf
@@ -5,13 +5,13 @@ terraform {
       version = "4.80.0"
     }
   }
-  #   backend "azurerm" {
-  #     resource_group_name  = "rg-test"
-  #     storage_account_name = "aqibstorageaccount1212"
-  #     container_name       = "aqibcontainer"
-  #     key                  = "dev.tfstate"
-  #  use_azuread_auth = true
-  #   }
+    backend "azurerm" {
+      resource_group_name  = "rg-test"
+      storage_account_name = "aqibstorageaccount1212"
+      container_name       = "aqibcontainer"
+      key                  = "dev.tfstate"
+   use_azuread_auth = true
+    }
 }
 
 provider "azurerm" {
